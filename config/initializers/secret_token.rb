844b930ff3dfe2deb29472726547de806c96d68c
nicklombardi <nicklombardi@gmail.com>
2013-07-17 09:37:03 -0400

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsCloud::Application.config.secret_token = 'eeb3dfec8fed827df563d681002f4e5403ffead0ba1cb71a3280b8971819a354e5d900f61b4713527a257c1fb78fb905081cab9ea756899c03877a68b48fe930'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NewsCloud::Application.config.session_store :cookie_store, key: '_NewsCloud_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NewsCloud::Application.config.session_store :active_record_store
