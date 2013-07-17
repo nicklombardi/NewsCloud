866ccba8a343222dba3f468830a152d9c2a1110d
nicklombardi <nicklombardi@gmail.com>
2013-07-17 11:09:25 -0400

    combined heat map and articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,43 @@
 class HomeController < ApplicationController
+
   def index
+    content = open("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=e1e84aebb55e5ce8373c0ab00071b821:7:67872659").read
+
+    @articles = JSON.parse(content)['results']
+
+    newswire_content = open("http://api.nytimes.com/svc/news/v3/content/all/all/.json?api-key=ae3c6baac9d440ef0696435d6aaacfb3:2:67872659").read
+
+    @newswire_articles = JSON.parse(newswire_content)['results']
+
+    @geolocations = []
+
+    @newswire_articles.each do |article|
+      @geolocations << article['geo_facet']
+    end
+
+    newswire_content_two = open("http://api.nytimes.com/svc/news/v3/content/all/all/.json?&offset=20&api-key=ae3c6baac9d440ef0696435d6aaacfb3:2:67872659").read
+
+    @newswire_articles_two = JSON.parse(newswire_content_two)['results']
+
+    @newswire_articles_two.each do |article|
+      @geolocations << article['geo_facet']
+    end
+
+    newswire_content_three = open("http://api.nytimes.com/svc/news/v3/content/all/all/.json?&offset=40&api-key=ae3c6baac9d440ef0696435d6aaacfb3:2:67872659").read
+
+    @newswire_articles_three = JSON.parse(newswire_content_three)['results']
+
+    @newswire_articles_three.each do |article|
+      @geolocations << article['geo_facet']
+    end
+
+  end
+
+  def view
+   render do|f|
+    f.html
+    f.js
+   end
   end
 
   def geocoded_clicks
---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,23 @@
+module HomeHelper
+
+  def format(string)
+    string.gsub("&#8216;", "\'").gsub("&#8217;", "\'").gsub("&#8220;", "\"").gsub("&#8221;", "\"")
+  end
+
+  def paren_removal(paren_string)
+    paren_string.gsub("(", "").gsub(")", "")
+  end
+
+  # def google_maps_api_key
+  #   "AIzaSyAkc8i9GKFIZppGA4s3N9oZmp-Wwq7PR6E"
+  # end
+
+  # def google_api_url
+  #   "http://maps.googleapis.com/maps/api/js"
+  # end
+
+  # def google_api_access
+  #   "#{google_api_url}?key=#{AIzaSyAkc8i9GKFIZppGA4s3N9oZmp-Wwq7PR6E}&libraries=geometry&sensor=false"
+  # end
+
+end
